fac4b4ce21f3db1a6aa7cb763e6e36fccf1b4fc1
ewsbr <ewsbr@outlook.com>
2024-07-17 20:29:18 -0300

    feat: add a favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,5 +22,11 @@ export default defineNuxtConfig({
     }
   },
 
+  app: {
+    head: {
+      link: [{ rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }],
+    }
+  },
+
   compatibilityDate: '2024-07-15'
 })
